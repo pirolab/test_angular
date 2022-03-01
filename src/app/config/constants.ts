4578d00c462ef577d1a0d64b46a10ef5393fbeea
Diego <dvalobra@hotelbeds.com>
2022-03-01 20:25:50 +0100

    Update constants.ts

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -6,6 +6,6 @@ export class Constants {
   public static API_ENDPOINT_DETAIL: string = 'https://hotels4.p.rapidapi.com/properties/get-details';
   public static API_ENDPOINT_DETAIL_IMAGES: string = 'https://hotels4.p.rapidapi.com/properties/get-hotel-photos';
   public static API_BASE_URL: string = 'hotels4.p.rapidapi.com';
-  public static API_AUTH_KEY: string = 'your KEY';
+  public static API_AUTH_KEY: string = 'd789f1c06emsha9868d10ea7cd1bp1548bbjsna1681535534c';
   public static API_DEST_ID: number = 10233105;
 }
